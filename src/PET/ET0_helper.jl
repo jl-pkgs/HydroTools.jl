328aa20e678cbc982874b5b6381e5599769f9efc
Dongdong Kong <kongdd@users.noreply.github.com>
2024-09-24 09:15:56 +0800

    rm temp files

--- src/PET/ET0_helper.jl
+++ src/PET/ET0_helper.jl
@@ -67,25 +67,4 @@ function cal_TvK(Tair, q)
 end
 
 
-"""
-    aerodynamic_conductance(U2, hc)
-
-# Arguments
-- `U2`: wind speed at 2m
-- `hc`: canopy height
-
-# Return
-- `Ga`: aerodynamic conductance in m/s
-"""
-function aerodynamic_conductance(U2::T, hc::T; Zob=15.0) where {T<:Real}
-  kmar = 0.40        # von Karman's constant 0.40
-  d = 0.64 * hc
-  zom = 0.13 * hc
-  zoh = 0.10 * zom
-  uz = cal_Uz(U2, Zob) # convert from u2 to uz
-  @fastmath Ga = uz * kmar^2 / (log((Zob - d) / zom) * log((Zob - d) / zoh)) # m s-1
-  Ga
-end
-
-
 export cal_Uz, cal_U2, cal_Pa, cal_lambda, cal_gamma, cal_slope, cal_bowen, mol2m
